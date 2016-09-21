ca9c41c0ebad9e708a0089e35ee874c5ec86b5ba
Development Imac <Designer@Mike-Whites-iMac.local>
2016-09-21 10:25:20 -0400

    Updated template

--- columbianissan/assets/js/script.js
+++ columbianissan/assets/js/script.js
@@ -1,1 +1,12 @@
 // DICK SMITH COLUMBIA NISSAN
+
+// HEADER - RESPONSIVE MENU FUNCTION
+$(function() {
+	var $menuButton = $("#header-mobile-button");
+	var $list = $("#header-nav-list");
+
+	$menuButton.on("click", function(e) {
+		$list.toggleClass("active");
+		e.preventDefault();
+	});
+});
